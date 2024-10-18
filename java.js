1746fcb87e8a5e19a2b675ae6d7b17a3b1635d65
Pineapple4u <165797715+Pineapple4u@users.noreply.github.com>
2024-10-18 16:38:46 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+var name0 = "Hellooo, My name is: " 
+var name1 = "Aarna";
+var name2 = "Kapuria";
+ 
+var merged_names = name0.concat(name1, " ", name2);
+document.getElementById("display").innerHTML = merged_names
